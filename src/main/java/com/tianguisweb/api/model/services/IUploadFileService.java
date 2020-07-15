58ab6cb6d09f3e3d412b12da442e0ac29d929ba1
Erick Gonzalez <erickgozan@gmail.com>
2020-07-14 23:45:18 -0500

    Se agrego la interfaz IUploadFileService para subir las imagenes

---
+++ src/main/java/com/tianguisweb/api/model/services/IUploadFileService.java
@@ -0,0 +1,24 @@
+package com.tianguisweb.api.model.services;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Path;
+
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+public interface IUploadFileService {
+
+	// Subir el archivo
+	public String uploadFile(MultipartFile nombreArchivo) throws IOException;
+
+	// Evalua si el archivo existe
+	public boolean isExist(String nombreArchivoAnterior);
+
+	// Visualiza el archivo
+	public Resource viewFile(String nombreArchivo) throws MalformedURLException;
+
+	// Retorna la ruta absoluta del archivo
+	public Path filePath(String nombreArchivo);
+
+}
