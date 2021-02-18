e7e1090666515252a146f4b1f35101711a6b91e1
erick <erickgozan@gmail.com>
2021-02-18 00:02:25 -0600

    Se creo un metodo para listar las categorias

--- src/main/java/com/tianguisweb/api/model/daos/IProductoDao.java
+++ src/main/java/com/tianguisweb/api/model/daos/IProductoDao.java
@@ -1,9 +1,18 @@
 package com.tianguisweb.api.model.daos;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.tianguisweb.api.model.entities.Categoria;
 import com.tianguisweb.api.model.entities.Producto;
 
 public interface IProductoDao extends JpaRepository<Producto, Long>{
-
+		
+	@Query("FROM Categoria")
+	public List<Categoria> findAllCategorias();
+	
+	public List<Producto> findProductoByCategoria(Categoria categoria);
+	
 }
