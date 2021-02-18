b9421b00f91c2dccda4f4d53b980561a81bf98f4
erick <erickgozan@gmail.com>
2021-02-18 00:11:01 -0600

    Se cambio de ubicacion las implemetaciones de las interfaces

---
+++ src/main/java/com/tianguisweb/api/model/servicesImpl/ProductoServiceImpl.java
@@ -0,0 +1,61 @@
+package com.tianguisweb.api.model.servicesImpl;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tianguisweb.api.model.daos.IProductoDao;
+import com.tianguisweb.api.model.entities.Categoria;
+import com.tianguisweb.api.model.entities.Producto;
+import com.tianguisweb.api.model.services.IProductoService;
+
+@Service
+@Transactional
+public class ProductoServiceImpl implements IProductoService{
+	
+	//Inyecta la interfaz que tiene los metodos CRUD
+	@Autowired
+	private IProductoDao productoDao;
+	
+	//Retorna la lista de productos
+	@Override
+	public List<Producto> findAllProductos() {
+		return productoDao.findAll();
+	}
+	
+	//Retorna la lista de categorias
+	@Override
+	public List<Categoria> finAllCategorias() {
+		return productoDao.findAllCategorias();
+	}
+	
+	//Retorna la lista de productos que tengan la categoria en comun
+	@Override
+	public List<Producto> findProductoByCategoria(Categoria categoria) {
+		return this.productoDao.findProductoByCategoria(categoria);
+	}
+
+	//Retorna el producto en base a su id
+	@Override
+	public Producto findProductoById(Long id) {
+		return productoDao.findById(id).orElse(null);
+	}
+
+	//Guarda el producto
+	@Override
+	public Producto saveProducto(Producto producto) {
+		return productoDao.save(producto);
+	}
+
+	//Elimina el producto
+	@Override
+	public void deleteProducto(Producto producto) {
+		productoDao.delete(producto);
+	}
+
+	
+
+}
---
+++ src/main/java/com/tianguisweb/api/model/servicesImpl/UploadFileServiceImpl.java
@@ -0,0 +1,97 @@
+package com.tianguisweb.api.model.servicesImpl;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.tianguisweb.api.model.services.IUploadFileService;
+
+@Service
+public class UploadFileServiceImpl implements IUploadFileService {
+	
+	private static final Logger log = LoggerFactory.getLogger(UploadFileServiceImpl.class);
+
+	@Override
+	public String uploadFile(MultipartFile nombreArchivo) throws IOException {
+
+		String filename = UUID.randomUUID().toString().concat("_").concat(nombreArchivo.getOriginalFilename())
+				.replace(" ", "_");
+		Path rutaArchivo = filePath(filename);
+
+		if (!nombreArchivo.isEmpty()) {
+			Files.copy(nombreArchivo.getInputStream(), rutaArchivo);
+		}
+		
+		return filename;
+	}
+
+	@Override
+	public boolean isExist(String nombreArchivoAnterior) {
+
+		if (nombreArchivoAnterior != null) {
+			Path rutaAnterior = Paths.get("uploads/products").resolve(nombreArchivoAnterior).toAbsolutePath();
+			File archivoAnterior = rutaAnterior.toFile();
+			if (archivoAnterior.exists() && archivoAnterior.canRead()) {
+				return archivoAnterior.delete();
+			}
+		}
+		
+		return false;
+	}
+	
+	@Override
+	public Resource viewFile(String nombreArchivo) throws MalformedURLException {
+
+		Path ruta = filePath(nombreArchivo);
+		Resource recurso = new UrlResource(ruta.toUri());
+
+		if (!recurso.exists() && !recurso.isReadable()) {
+			ruta = Paths.get("src/main/resources/static/images").resolve("no-user.png").toAbsolutePath();
+			recurso = new UrlResource(ruta.toUri());
+		}
+
+		return recurso;
+	}
+
+	//Establecer la ruta del o los archivos
+	@Override
+	public Path filePath(String nombreArchivo) {
+		
+		File carpeta = Paths.get("uploads/products").toAbsolutePath().toFile();
+		if (!carpeta.exists()) {
+			carpeta.mkdir();
+		}
+		log.info("La ruta es "+carpeta.toString());
+		
+		return carpeta.toPath().resolve(nombreArchivo);
+	}
+}
+
+
+
+
+
+/*forma alternativa de agregar la ruta de user dir 
+ * Path rutaArchivoAnterior = Paths.get("uploads".concat(File.separator).concat("products"))
+ * .toAbsolutePath().resolve(nombreArchivoAnterior).toAbsolutePath();
+ * 
+ * 	/*@Override
+ /*Metodo alternativo para subir un arreglo de imagenes
+public void saveAll(List<MultipartFile> files) throws IOException {
+	// TODO Auto-generated method stub
+	for (MultipartFile file : files) {
+	     this.uploadFile(file);
+	}
+}*/	
+
