bb0fd784a3591ced6d9439c539f430e824f1dbf1
erick <erickgozan@gmail.com>
2021-02-18 00:03:57 -0600

    Se crearon los metods findAllCategorias() y findProductoByCategoria()

--- src/main/java/com/tianguisweb/api/model/services/IProductoService.java
+++ src/main/java/com/tianguisweb/api/model/services/IProductoService.java
@@ -2,12 +2,19 @@ package com.tianguisweb.api.model.services;
 
 import java.util.List;
 
+import com.tianguisweb.api.model.entities.Categoria;
 import com.tianguisweb.api.model.entities.Producto;
 
 public interface IProductoService {
 
 	// Retornar el listado completo de productos
 	public List<Producto> findAllProductos();
+	
+	//Retornar el listado de categorias
+	public List<Categoria> finAllCategorias();
+	
+	//Retorna los productos pertenecientes a cierta categoria
+	public List<Producto> findProductoByCategoria(Categoria categoria);	
 
 	// Retornar el producto por su id
 	public Producto findProductoById(Long id);
