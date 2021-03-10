178e800803db21aff786b6f4a260a047d58b97aa
erick <erickgozan@gmail.com>
2021-03-10 16:50:15 -0600

    Se agrego la etiqueta @Repository al dao del la categoria

--- src/main/java/com/tianguisweb/api/model/daos/ICategoriaDao.java
+++ src/main/java/com/tianguisweb/api/model/daos/ICategoriaDao.java
@@ -1,10 +1,12 @@
 package com.tianguisweb.api.model.daos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.tianguisweb.api.model.entities.Categoria;
 
-public interface ICategoriaDao extends JpaRepository<Categoria, Long> {
+@Repository
+public interface ICategoriaDao extends JpaRepository<Categoria, String> {
 	
 	//public List<Categoria> findByCategoria(Categoria categoria);
 	
