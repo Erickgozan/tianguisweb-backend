c4cd24e3d5bbc444240b634dd0a3515c5623833a
erick <erickgozan@gmail.com>
2021-03-10 16:48:58 -0600

    Se creo el servicio de la direccion

---
+++ src/main/java/com/tianguisweb/api/model/servicesImpl/DireccionServiceImpl.java
@@ -0,0 +1,45 @@
+package com.tianguisweb.api.model.servicesImpl;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tianguisweb.api.model.daos.IDireccionDao;
+import com.tianguisweb.api.model.entities.Direccion;
+import com.tianguisweb.api.model.services.IDireccionService;
+
+@Transactional
+@Service
+public class DireccionServiceImpl implements IDireccionService {
+
+	@Autowired
+	private IDireccionDao direccionesDao;
+
+	// Listar direcciones
+	@Override
+	public List<Direccion> findAllDirecciones() {
+		return this.direccionesDao.findAll();
+	}
+
+	// Guardar la direccion
+	@Override
+	public Direccion saveDireccion(Direccion direccion) {
+		return this.direccionesDao.save(direccion);
+	}
+
+	// Buscar la direccion por su id
+	@Override
+	public Direccion findDireccionById(String id) {
+		return this.direccionesDao.findById(id).orElse(null);
+	}
+
+	// Eliminar la direccion
+	@Override
+	public void delateDireccion(Direccion direccion) {
+		this.direccionesDao.delete(direccion);
+	}
+
+}
