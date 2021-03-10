febb00582a79be21bbd9ffae9da4e8b88f32a793
erick <erickgozan@gmail.com>
2021-03-10 16:54:29 -0600

    Se agrego la etiqueta @Repository

--- src/main/java/com/tianguisweb/api/model/daos/IProductoDao.java
+++ src/main/java/com/tianguisweb/api/model/daos/IProductoDao.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.tianguisweb.api.model.entities.Categoria;
 import com.tianguisweb.api.model.entities.Producto;
 
-public interface IProductoDao extends JpaRepository<Producto, Long>{
+@Repository
+public interface IProductoDao extends JpaRepository<Producto, String>{
 		
 	@Query("FROM Categoria")
 	public List<Categoria> findAllCategorias();
