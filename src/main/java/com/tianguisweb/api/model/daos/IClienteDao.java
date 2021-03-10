77e400541b27a8637439ebd88f994bdc4205b0bd
erick <erickgozan@gmail.com>
2021-03-10 16:51:35 -0600

    Se agrego la etiqueta @Repository

--- src/main/java/com/tianguisweb/api/model/daos/IClienteDao.java
+++ src/main/java/com/tianguisweb/api/model/daos/IClienteDao.java
@@ -1,9 +1,11 @@
 package com.tianguisweb.api.model.daos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.tianguisweb.api.model.entities.Cliente;
 
-public interface IClienteDao extends JpaRepository<Cliente, Long>{
+@Repository
+public interface IClienteDao extends JpaRepository<Cliente, String>{
 
 }
