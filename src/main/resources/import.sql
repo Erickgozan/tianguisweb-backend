9bceb49575cd839ce0702c9cdb81a2dfd8ada2ce
Erick <Erick@DESKTOP-JAHLT62>
2021-03-16 22:39:34 -0600

    Se insertaron los roles

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,6 @@
-insert into categorias(id,nombre) values(1,"Tecnologia");
-insert into categorias(id,nombre) values(2,"Hogar");
+insert into categorias(id,nombre) values("b07ec09a-4528-4777-86d1-2c09ab3b3cf7","Tecnologia");
+insert into categorias(id,nombre) values("a87d4dd9-da79-45f1-af53-cab63b60d21c","Hogar");
 
+
+insert into roles(id,nombre) values ("981c850d-f14e-45f7-96e4-dc69dac49dbb","ROLE_USER");
+insert into roles(id,nombre) values("70e6b20a-b28d-47b3-80cb-0fac05db4e07","ROLE_ADMIN");
