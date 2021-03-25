3a6083c7ada15e3b28e7c6e4fc62725504217625
Erick <erickgozan@gmail.com>
2021-03-24 19:13:50 -0600

    Se agrego la anotacion @Transactional de spring

--- src/main/java/com/tianguisweb/api/model/servicesImpl/CategoriaServiceImpl.java
+++ src/main/java/com/tianguisweb/api/model/servicesImpl/CategoriaServiceImpl.java
@@ -1,9 +1,9 @@
 package com.tianguisweb.api.model.servicesImpl;
 
-import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.tianguisweb.api.model.daos.ICategoriaDao;
 import com.tianguisweb.api.model.entities.Categoria;
