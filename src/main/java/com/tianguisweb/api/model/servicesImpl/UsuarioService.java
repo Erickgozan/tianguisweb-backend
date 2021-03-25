cd1e0de9d0de7d2cd6de44ebdfdde1d8fb571699
Erick <erickgozan@gmail.com>
2021-03-24 19:24:18 -0600

    clase que me retorna el cliente/usuario autenticado

---
+++ src/main/java/com/tianguisweb/api/model/servicesImpl/UsuarioService.java
@@ -0,0 +1,54 @@
+package com.tianguisweb.api.model.servicesImpl;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.tianguisweb.api.model.daos.IUsuarioDao;
+import com.tianguisweb.api.model.entities.Usuario;
+
+@Transactional
+@Service
+@Primary
+public class UsuarioService implements UserDetailsService{
+
+	private final static  Logger LOG = LoggerFactory.getLogger(UsuarioService.class); 
+	
+	@Autowired
+	private IUsuarioDao usuarioDao;
+
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		
+		Usuario usuario = this.usuarioDao.findByUsername(username);
+		
+		if(usuario==null) {
+			LOG.error("Error en el login no existe el usuario: "+username+" en el sistema");
+			throw new UsernameNotFoundException("Error en el login no existe el usuario: "+username+" en el sistema");
+		}
+		
+		List<GrantedAuthority> authorities = usuario.getRoles()
+				.stream()
+				.map(rol -> new SimpleGrantedAuthority(rol.getNombre()))
+				.peek(authority -> LOG.info("Role: " + authority.getAuthority()))
+				.collect(Collectors.toList());
+		
+		return new User(usuario.getUsername(), usuario.getPassword(),
+				usuario.getHabilitado(), true, true, true, authorities);
+	}
+	
+}
