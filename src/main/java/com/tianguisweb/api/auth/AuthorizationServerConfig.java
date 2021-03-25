5ea8a7b0c5f340c61355cd02872f3114ec019660
Erick <erickgozan@gmail.com>
2021-03-24 19:12:49 -0600

    Clases de configuracion de la seguridad

---
+++ src/main/java/com/tianguisweb/api/auth/AuthorizationServerConfig.java
@@ -0,0 +1,80 @@
+package com.tianguisweb.api.auth;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+
+//servidor de autorización responsable de administrar los tokens de acceso
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter{
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+
+	@Autowired
+	private InfoAdicionalToken infoAdicionalToken;
+	
+	@Autowired
+	@Qualifier("authenticationManager")
+	private AuthenticationManager authenticationManager;
+
+	//Permisos de las rutas de acceso
+	@Override
+	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+		//cualquier usuario pueda autenticarse y genera el token
+		security.tokenKeyAccess("permitAll()").
+		//Valida el token
+		checkTokenAccess("isAuthenticated()");
+	}
+
+	//Registro del cliente que consumira la aplicacion (Angular)
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		clients.inMemory().withClient("angularApp").
+		secret(passwordEncoder.encode("EA_gf_1240@")).
+		scopes("read","write")
+		//Tipo de autorizacion del usuario (password) y se actualizara el token
+		.authorizedGrantTypes("password","refresh_token")
+		.accessTokenValiditySeconds(3600)
+		.refreshTokenValiditySeconds(3600);
+	}
+	
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+			
+		//Agregar informacion adicional al token
+		TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain();
+		tokenEnhancerChain.setTokenEnhancers(Arrays.asList(infoAdicionalToken,accessTokenConverter()));
+		
+		endpoints.authenticationManager(authenticationManager)
+		.accessTokenConverter(accessTokenConverter()).tokenEnhancer(tokenEnhancerChain);
+	
+	}
+	
+	//Establece el tipo de algoritmo para firmar JWT ya se mac o RS256
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		
+		JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
+		jwtAccessTokenConverter.setSigningKey(JwtKey.RSA_PRIVATE);
+		jwtAccessTokenConverter.setVerifierKey(JwtKey.RSA_PUBLIC);
+		
+		return jwtAccessTokenConverter;
+	}
+	
+	
+	
+}
---
+++ src/main/java/com/tianguisweb/api/auth/SpringSecurityConfig.java
@@ -0,0 +1,55 @@
+package com.tianguisweb.api.auth;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+//servidor de autorización.
+//Cuando la aplicación cliente necesita adquirir un token de acceso,
+//lo hará después de un simple proceso de autenticación impulsado por un formulario de inicio de sesión
+@Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true)
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	//Implementa la clase UsuarioService que contiene los datos del usuario
+	@Autowired
+	@Qualifier("usuarioService")
+	private UserDetailsService usuarioService;
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+			return new BCryptPasswordEncoder();
+	}
+	
+	@Override
+	@Autowired
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(this.usuarioService).passwordEncoder(passwordEncoder());
+	}
+
+	@Override
+	@Bean
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.authorizeRequests()
+		.anyRequest()
+		.authenticated().and().csrf().disable()
+		.sessionManagement()
+	    .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+
+}
