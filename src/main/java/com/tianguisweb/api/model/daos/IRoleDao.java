abd86262ce7eb75a5dcb5ed192a1bd0aced377ae
Erick <erickgozan@gmail.com>
2021-04-10 16:18:22 -0500

    Se agrego el metodo findByRoleNombre(String nombre)

--- src/main/java/com/tianguisweb/api/model/daos/IRoleDao.java
+++ src/main/java/com/tianguisweb/api/model/daos/IRoleDao.java
@@ -1,5 +1,7 @@
 package com.tianguisweb.api.model.daos;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.tianguisweb.api.model.entities.Role;
 
 @Repository
 public interface IRoleDao extends JpaRepository<Role, String>{
+	
+	public List<Role> findByRoleNombre(String nombre);
 
 }
