5fee68884dab461fd40690be8c83a1d94cbbe129
Erick <erickgozan@gmail.com>
2021-04-10 16:26:00 -0500

    Se agregaron las rutas /oauth/google y /oauth/facebook

--- src/main/java/com/tianguisweb/api/auth/ResourceServerConfig.java
+++ src/main/java/com/tianguisweb/api/auth/ResourceServerConfig.java
@@ -33,8 +33,8 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 		.permitAll()
 		.antMatchers(HttpMethod.POST,
 		"/api/clientes/create",
-		"/api/oauth/google",
-		"/api/oauth/facebook")
+		"/oauth/google",
+		"/oauth/facebook")
 		.permitAll()
 		.anyRequest()
 		.authenticated()
