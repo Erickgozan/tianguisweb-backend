47ed6a26a9bb8b9ef83c4785183c31f2de9c8c55
Erick <erickgozan@gmail.com>
2021-04-10 16:20:57 -0500

    Se creo la clase JwtKey

---
+++ src/main/java/com/tianguisweb/api/auth/JwtKey.java
@@ -0,0 +1,44 @@
+package com.tianguisweb.api.auth;
+
+public class JwtKey {
+
+	
+	public static final String RSA_PRIVATE="-----BEGIN RSA PRIVATE KEY-----\r\n"
+			+ "MIIEogIBAAKCAQEA3z6ruz3YBGarSqJAMpktUNHvz0DMhrWquhss3Q2mpZFrDd7s\r\n"
+			+ "GrTQhHlT/hbWtw0xMjDfQfaHFxQtr45rPJZReYt2cEh6bn0ArYueXdfpedt6qXz7\r\n"
+			+ "3FPGNhTHujRgd+AeMOQvgrsxkYMvjQmzqnlPGHqKtXgXgnuUjYxb1pr11ltc5z3J\r\n"
+			+ "03YD73Y+HmFwFX8+TXk6sFuiNS5KHlJr+Q56tqslPhuyKLXE3P9N/o7TbZAjwCYc\r\n"
+			+ "w2FPu6vRk3uetnGia31tBXpImdeOo0IYahwVwSfJNS9mSnWlbAG5XkKM0EE+11Mi\r\n"
+			+ "toDQoGsWkMtJZujgCM7jcSDv9PmOdPO1tOU9qQIDAQABAoIBAArlpT5Ti3rThCL1\r\n"
+			+ "foq5vWOHi9wi/pjIv1esQelcZDkKux4HNqErGE6PGZF6jeqLg04+4nb20KtIud4q\r\n"
+			+ "5oMjl0CGlTZv40IHagQOn6FXC7d+BiPFzQl5MWCsbP9i1R6k2vRp/wOznXzL2YNZ\r\n"
+			+ "6OI2X60DNkgbX5Jn891g746vK7bV0XRWg5RCoZet2jhhej2+9TIK5QL9egTCx+8H\r\n"
+			+ "j3hV1dpDa0L0xI5A1y3GcPkTv7FGbrtAM7RHW97dVqTWsyRQmaUuWO9i82YXUkg8\r\n"
+			+ "FAyY6RuCcG9TDz1Q9vef+qN1gcq4MGFZqNundVZSCTVKPaJ++bl/YsJWdqWi3A7Y\r\n"
+			+ "36kyKHECgYEA/2sGJIW2QqYen7WPOnj1uuylAJun46P2Aqiy7ZQ34p/MtZO9oPti\r\n"
+			+ "ExMYllqlqWUIKKI3TLn7eOSSSU+fan4FnCyM2bA5y/+KoXihKepZZkSS+eAsHBo+\r\n"
+			+ "P9YXWa8tCDQmE7skLUcplFmTpNmVY2iJzXi4fDUIJSG4Ua2m/Rpm7z0CgYEA38Dh\r\n"
+			+ "oB7B7W96hIrvm92U4aXNqRW0EFV1CuVMY0YVjwniE0lf8l2hXTulrVsHwI72kzCV\r\n"
+			+ "giJH0WFyPUR5DfefUtjokFA4Lmo6VKyj8yqd+kbCv6qFIBCrkghGip9Otz9b0hRB\r\n"
+			+ "/O6inqUpRHm3JRMMjJAooN/4YiT/9iPQZs/27t0CgYBOMBb7SytWD8BEJ7ImtiyT\r\n"
+			+ "Aioom32zg9elTLspWzwYVc62rvlXtFx7dlcKf8lCp7jtp2rCRBSlF2NoqcUlNlrF\r\n"
+			+ "SiX75aRrvFhVwbWkPScM1WUgtO0L1UTFCeav/54LattVIu6aJbSmwx0dU7YVbkTw\r\n"
+			+ "70maTCmoJEBmUIBT7qcgCQKBgEb3dBhG6RcXjnJPZqO26auP3OTN/2hW0mJ8S3gJ\r\n"
+			+ "GHlflxaq5QWH8NATRP7wqkrGjSot9J1rpf+guoz4FLT7Xsr2BP0QlGEUTB/jCbTs\r\n"
+			+ "RWLF4qrLuKFs6B3PBiCdWmgAbmz5A9znCyjqFWakfBQEMKy8ZEZyAuON/Kjivwx8\r\n"
+			+ "1UL5AoGAaDL8MF0/4Yu++FN4K1T5FMk+0kUULo8k13hgVmUENDwVE6t/Fhc3krct\r\n"
+			+ "fiiaEId3vaUAf2lE5evqs9gv24y1yAUS7Uco/SLpLM8ZiyJOxXVSVFkTxPraaKdr\r\n"
+			+ "KAF1gQO/Inj/HIIUMjgEENiGQUnHbbTVevxnWswkjOa10A/P/Hg=\r\n"
+			+ "-----END RSA PRIVATE KEY-----";
+	
+	public static final String RSA_PUBLIC="-----BEGIN PUBLIC KEY-----\r\n"
+			+ "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA3z6ruz3YBGarSqJAMpkt\r\n"
+			+ "UNHvz0DMhrWquhss3Q2mpZFrDd7sGrTQhHlT/hbWtw0xMjDfQfaHFxQtr45rPJZR\r\n"
+			+ "eYt2cEh6bn0ArYueXdfpedt6qXz73FPGNhTHujRgd+AeMOQvgrsxkYMvjQmzqnlP\r\n"
+			+ "GHqKtXgXgnuUjYxb1pr11ltc5z3J03YD73Y+HmFwFX8+TXk6sFuiNS5KHlJr+Q56\r\n"
+			+ "tqslPhuyKLXE3P9N/o7TbZAjwCYcw2FPu6vRk3uetnGia31tBXpImdeOo0IYahwV\r\n"
+			+ "wSfJNS9mSnWlbAG5XkKM0EE+11MitoDQoGsWkMtJZujgCM7jcSDv9PmOdPO1tOU9\r\n"
+			+ "qQIDAQAB\r\n"
+			+ "-----END PUBLIC KEY-----";
+	
+}
