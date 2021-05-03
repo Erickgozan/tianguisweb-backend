3f48e3d71cace75d71c8987a87e4235a5a353583
Erick Gonzalez <erickgozan@gmail.com>
2021-05-03 13:57:24 -0500

    Se modifico el metodo isExist y se agregaron delateAllFiles y saveAll

--- src/main/java/com/tianguisweb/api/model/servicesImpl/UploadFileServiceImpl.java
+++ src/main/java/com/tianguisweb/api/model/servicesImpl/UploadFileServiceImpl.java
@@ -6,12 +6,16 @@ import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
+//import java.util.stream.Stream;
 
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.FileSystemUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.tianguisweb.api.model.services.IUploadFileService;
@@ -19,85 +23,93 @@ import com.tianguisweb.api.model.services.IUploadFileService;
 @Service
 @Transactional
 public class UploadFileServiceImpl implements IUploadFileService {
+
+	private final Path rootFolder = Paths.get("uploads/products");
 	
-	//private static final Logger log = LoggerFactory.getLogger(UploadFileServiceImpl.class);
-	
-	//Metodo para subir la imagen
+	//private final Path productFolder = Paths.get("products");	
+	/*private final Path sliderFolder = Paths.get("slider");*/
+
+	// Metodo para subir la imagen
 	@Override
 	public String uploadFile(MultipartFile nombreArchivo) throws IOException {
-		
-		//Genera un id a la imagen 
+
+		// Genera un id a la imagen
 		String filename = UUID.randomUUID().toString().concat("_").concat(nombreArchivo.getOriginalFilename())
 				.replace(" ", "_");
-		
+
 		Path rutaArchivo = filePath(filename);
 
 		if (!nombreArchivo.isEmpty()) {
 			Files.copy(nombreArchivo.getInputStream(), rutaArchivo);
 		}
-		
+
 		return filename;
 	}
-	
-	//Metodo para eliminar la imagen del servidor, si esta ya existe 
+
+	// Metodo para subir varias imagenes
+	@Override
+	public List<String> saveAll(List<MultipartFile> files) throws IOException {
+		List<String> imgs = new ArrayList<String>();
+		for (MultipartFile file : files) {
+			imgs.add(this.uploadFile(file));
+		}
+		return imgs;
+	}
+
+
+	// Metodo para eliminar la imagen del servidor, si esta ya existe
 	@Override
 	public boolean isExist(String nombreArchivoAnterior) {
 
 		if (nombreArchivoAnterior != null) {
-			Path rutaAnterior = Paths.get("uploads/products").resolve(nombreArchivoAnterior).toAbsolutePath();
+			Path rutaAnterior =this.rootFolder.resolve(nombreArchivoAnterior)
+					.toAbsolutePath();
 			File archivoAnterior = rutaAnterior.toFile();
 			if (archivoAnterior.exists() && archivoAnterior.canRead()) {
 				return archivoAnterior.delete();
 			}
 		}
-		
+
 		return false;
 	}
-	
-	//Metodo para visualizar la imagen.
+
+	// Metodo para visualizar la imagen.
 	@Override
 	public Resource viewFile(String nombreArchivo) throws MalformedURLException {
 
 		Path ruta = filePath(nombreArchivo);
 		Resource recurso = new UrlResource(ruta.toUri());
 
-		if (!recurso.exists() && !recurso.isReadable()) {
-			ruta = Paths.get("src/main/resources/static/images").resolve("no-user.png").toAbsolutePath();
-			recurso = new UrlResource(ruta.toUri());
-		}
-
 		return recurso;
 	}
 
-	//Establecer la ruta del o los archivos
+	// Establecer la ruta del o los archivos
 	@Override
 	public Path filePath(String nombreArchivo) {
-		
-		
-		File carpeta = Paths.get("uploads/products").toAbsolutePath().toFile();
-		
+
+		File carpeta = this.rootFolder.toAbsolutePath().toFile();
 		if (!carpeta.exists()) {
 			carpeta.mkdirs();
-		}	
-		
+		}
 		return carpeta.toPath().resolve(nombreArchivo);
 	}
+
+	@Override
+	public void delateAllFiles() {
+		FileSystemUtils.deleteRecursively(this.rootFolder.toFile());
+	}
+
 }
 
-
-
-
-
-/*forma alternativa de agregar la ruta de user dir 
- * Path rutaArchivoAnterior = Paths.get("uploads".concat(File.separator).concat("products"))
+/*
+ * forma alternativa de agregar la ruta de user dir Path rutaArchivoAnterior =
+ * Paths.get("uploads".concat(File.separator).concat("products"))
  * .toAbsolutePath().resolve(nombreArchivoAnterior).toAbsolutePath();
  * 
- * 	/*@Override
- /*Metodo alternativo para subir un arreglo de imagenes
-public void saveAll(List<MultipartFile> files) throws IOException {
-	// TODO Auto-generated method stub
-	for (MultipartFile file : files) {
-	     this.uploadFile(file);
+ * 
+ * @Override
+	public Stream<Path> loadAll() throws Exception {
+		return null;
 	}
-}*/	
-
+ * 
+ */
