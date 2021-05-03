cf6b894a6d3920e0ec61a1d0ea8f09026db0661e
Erick Gonzalez <erickgozan@gmail.com>
2021-05-03 13:54:59 -0500

    Se agrego el metodo run para eliminar las imagenes al arrancar

--- src/main/java/com/tianguisweb/api/TianguiswebBackendApplication.java
+++ src/main/java/com/tianguisweb/api/TianguiswebBackendApplication.java
@@ -1,13 +1,26 @@
 package com.tianguisweb.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class TianguiswebBackendApplication {
+import com.tianguisweb.api.model.services.IUploadFileService;
 
+@SpringBootApplication
+public class TianguiswebBackendApplication implements CommandLineRunner {
+
+	
+	@Autowired
+	private IUploadFileService uploadService;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TianguiswebBackendApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		uploadService.delateAllFiles();		
 	}
 	
 }
