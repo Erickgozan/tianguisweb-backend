2c72bdfbd44288ace69cd6f253b859571bad68fb
Erick Gonzalez <erickgozan@gmail.com>
2021-05-03 13:49:42 -0500

    Se creao el metodo actualizarProductoStok

--- src/main/java/com/tianguisweb/api/model/servicesImpl/PedidoServiceImpl.java
+++ src/main/java/com/tianguisweb/api/model/servicesImpl/PedidoServiceImpl.java
@@ -7,38 +7,58 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.tianguisweb.api.model.daos.IPedidoDao;
+import com.tianguisweb.api.model.daos.IProductoDao;
 import com.tianguisweb.api.model.entities.Pedido;
+import com.tianguisweb.api.model.entities.Producto;
 import com.tianguisweb.api.model.services.IPedidoService;
 
 @Transactional
 @Service
 public class PedidoServiceImpl implements IPedidoService {
-	
+
 	@Autowired
 	private IPedidoDao pedidoDao;
-	
-	//Retorna el listado de los pedidos
+
+	@Autowired
+	private IProductoDao productoDao;
+
+	// Retorna el listado de los pedidos
 	@Override
 	public List<Pedido> finadAllPedidos() {
 		return this.pedidoDao.findAll();
 	}
 
-	//Guarda el pedido
+	// Guarda el pedido
 	@Override
 	public Pedido sevePedido(Pedido pedido) {
+		this.actualizarProductoStok(pedido);
 		return this.pedidoDao.save(pedido);
 	}
-	
-	//Busca el pedido por su id
+
+	// Busca el pedido por su id
 	@Override
 	public Pedido findPedidoById(String id) {
 		return this.pedidoDao.findById(id).orElse(null);
 	}
 
-	//Elimina el pedido
+	// Elimina el pedido
 	@Override
 	public void delatePedido(Pedido pedido) {
 		this.pedidoDao.delete(pedido);
+	}
+
+	// Metodo para actualizar el stok
+	private void actualizarProductoStok(Pedido pedido) {
+
+		Producto producto = new Producto();
+
+		pedido.getItemProductos().forEach(p -> {
+			producto.setId(p.getProducto().getId());
+			producto.setStock(p.getProducto().getStock() - p.getCantidad());
+		});
+
+		this.productoDao.updateStock(producto.getId(), producto.getStock());
+
 	}
 
 }
