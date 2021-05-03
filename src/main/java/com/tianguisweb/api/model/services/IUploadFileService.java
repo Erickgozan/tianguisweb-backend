daa63b7030a43d2ce20e7bff009d76b6131dd1d6
Erick Gonzalez <erickgozan@gmail.com>
2021-05-03 13:45:08 -0500

    Se agrego el meotod saveAll y delateAllFiles

--- src/main/java/com/tianguisweb/api/model/services/IUploadFileService.java
+++ src/main/java/com/tianguisweb/api/model/services/IUploadFileService.java
@@ -3,6 +3,8 @@ package com.tianguisweb.api.model.services;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.nio.file.Path;
+import java.util.List;
+//import java.util.stream.Stream;
 
 import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
@@ -16,12 +18,20 @@ public interface IUploadFileService {
 	public boolean isExist(String nombreArchivoAnterior);
 	
 	// Guardar multiples archivos
-	//public void saveAll(List<MultipartFile> files) throws IOException;
-
+	public List<String> saveAll(List<MultipartFile> files) throws IOException;
+	
+	//Vidaulizar todos los archivos
+	//public Stream<Path> loadAll() throws Exception;
+	
 	// Visualiza el archivo
 	public Resource viewFile(String nombreArchivo) throws MalformedURLException;
 
 	// Retorna la ruta absoluta del archivo
 	public Path filePath(String nombreArchivo);
+	
+	//Metodos para eliminar los archivos *Solo aplicar en desarrollo
+	public void delateAllFiles();
+	
+
 
 }
