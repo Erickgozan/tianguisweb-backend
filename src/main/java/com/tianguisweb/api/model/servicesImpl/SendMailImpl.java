898a89b86d3b1e17031cf39f4a086b8a021d7e27
Erick Gonzalez <erickgozan@gmail.com>
2021-07-21 22:33:52 -0500

    Se creo la clase SendEmailImpl que envia el email

--- src/main/java/com/tianguisweb/api/model/servicesImpl/SendMailImpl.java
+++ src/main/java/com/tianguisweb/api/model/servicesImpl/SendMailImpl.java
@@ -1,2 +1,26 @@
-package com.tianguisweb.api.model.servicesImpl;public class SendMailImpl {
+package com.tianguisweb.api.model.servicesImpl;
+
+import com.tianguisweb.api.model.services.ISendMailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SendMailImpl implements ISendMailService {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @Override
+    public void sendMail(String from, String to, String subject, String body) {
+
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setFrom(from);
+        mailMessage.setTo(to);
+        mailMessage.setSubject(subject);
+        mailMessage.setText(body);
+
+        javaMailSender.send(mailMessage);
+    }
 }
