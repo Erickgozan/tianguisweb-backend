5ce8a7212be4802e28fc56373749b639384950b8
Erick Gonzalez <erickgozan@gmail.com>
2021-07-21 22:20:44 -0500

    See creo el metodo updateUsuarioPassword

--- src/main/java/com/tianguisweb/api/model/daos/IClienteDao.java
+++ src/main/java/com/tianguisweb/api/model/daos/IClienteDao.java
@@ -1,6 +1,10 @@
 package com.tianguisweb.api.model.daos;
 
+import com.tianguisweb.api.model.entities.Usuario;
+import org.hibernate.sql.Update;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.tianguisweb.api.model.entities.Cliente;
@@ -8,5 +12,9 @@ import com.tianguisweb.api.model.entities.Cliente;
 @Repository
 public interface IClienteDao extends JpaRepository<Cliente, String>{
 			
-	public Cliente findByUsername(String username);
+	Cliente findByEmail(String email);
+
+	@Modifying
+	@Query("UPDATE Usuario u SET u.password = :password WHERE u.id = :id")
+	void updateUsuarioPassword(String password, String id);
 }
