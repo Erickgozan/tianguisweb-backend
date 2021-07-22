7b2d55622c4125930c5e3b3cfb6b19a254d506c0
Erick Gonzalez <erickgozan@gmail.com>
2021-07-21 22:25:30 -0500

    Se agrego la interfaz para enviar el email

--- src/main/java/com/tianguisweb/api/model/services/ISendMailService.java
+++ src/main/java/com/tianguisweb/api/model/services/ISendMailService.java
@@ -1,23 +1,5 @@
 package com.tianguisweb.api.model.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.stereotype.Service;
-
-@Service
-public class SendMailService {
-
-    @Autowired
-    private JavaMailSender javaMailSender;
-
-    public void sendMail(String from,String to,String subject, String body){
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setFrom(from);
-        mailMessage.setTo(to);
-        mailMessage.setSubject(subject);
-        mailMessage.setText(body);
-
-        javaMailSender.send(mailMessage);
-    }
+public interface ISendMailService {
+    void sendMail(String from,String to,String subject, String body);
 }
