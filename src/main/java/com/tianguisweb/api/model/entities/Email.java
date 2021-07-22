0cb45650d7bf57c731b223173eeb9b17bfb92aa1
Erick Gonzalez <erickgozan@gmail.com>
2021-07-21 22:19:58 -0500

    Se creo la entidad Email

--- src/main/java/com/tianguisweb/api/model/entities/Email.java
+++ src/main/java/com/tianguisweb/api/model/entities/Email.java
@@ -1,2 +1,50 @@
-package com.tianguisweb.api.model.entities;public class Email {
+package com.tianguisweb.api.model.entities;
+
+public class Email {
+
+    private String url;
+    private String name;
+    private String mail;
+    private String subject;
+    private String body;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
 }
