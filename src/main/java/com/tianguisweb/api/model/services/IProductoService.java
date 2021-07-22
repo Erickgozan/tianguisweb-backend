04d2ece79b2281bc6106cf16a6b6475943205f7b
Erick Gonzalez <erickgozan@gmail.com>
2021-07-21 22:24:13 -0500

    Se agregaron todos los metodos para buscar los producos

--- src/main/java/com/tianguisweb/api/model/services/IProductoService.java
+++ src/main/java/com/tianguisweb/api/model/services/IProductoService.java
@@ -4,24 +4,39 @@ import java.util.List;
 
 import com.tianguisweb.api.model.entities.Categoria;
 import com.tianguisweb.api.model.entities.Producto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface IProductoService {
 
 	// Retornar el listado completo de productos
-	public List<Producto> findAllProductos();
+	 List<Producto> findAllProductos();
+
+	//Retornar el listado paginado de productos
+     Page<Producto> findAllPageProductos(Pageable pagina);
+
+	//Retornar el listado por nombre de producto
+	 List<Producto> findAllProductsByNombre(String nombre);
+
+	//Retornar el listado por nombre de producto
+	List<Producto> findAllProductosByDatos(String datos);
+
+
+	//Retorna los productos pertenecientes a cierta categoria
+	 List<Producto> findProductosByCategoria(Categoria categoria);
+
+	//Retorna los listados de productos que estan en oferta
+	List<Producto> findProductosByOferta(Boolean oferta);
 	
 	//Retornar el listado de categorias
-	public List<Categoria> finAllCategorias();
-	
-	//Retorna los productos pertenecientes a cierta categoria
-	public List<Producto> findProductoByCategoria(Categoria categoria);	
+	 List<Categoria> finAllCategorias();
 
 	// Retornar el producto por su id
-	public Producto findProductoById(String id);
+	 Producto findProductoById(String id);
 
 	// Guardar el producto
-	public Producto saveProducto(Producto producto);
+	 Producto saveProducto(Producto producto);
 
 	// Eliminar el producto
-	public void deleteProducto(Producto producto );
+	 void deleteProducto(Producto producto );
 }
